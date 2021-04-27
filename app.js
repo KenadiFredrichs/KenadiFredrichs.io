f106da38da677dc0aab81b44c020dfa0d436b07c
KenadiFredrichs <814769@mystma.org>
2021-04-27 18:12:24 +0000

    buttons

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const hov = document.getElementById("changeColor");
+const button = document.getElementById("countUp");
+const count = document.getElementById("count");
+const button = document.getElementById("countDown");
+var number =1;
 hov.addEventListener("mouseenter", 
     function()
     {hov.style.color = "white";
@@ -9,7 +13,23 @@ hov.addEventListener("mouseout",
     {hov.style.color = "black";
     hov.style.backgroundColor = "pink";}
 );
-hov.addEventListener("mouseclick", 
+
+hov.addEventListener("click", 
     function()
     {hov.style.fontSize = "64px";}
+);
+
+button.addEventListener("click",
+    function()
+    {
+        number=number+1;
+        count.innerHTML=number;
+    }
+);
+button.addEventListener("click",
+    function()
+    {
+        number=number-1;
+        count.innerHTML=number;
+    }
 );
